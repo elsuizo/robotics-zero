bdb8100d1172325a08286e9114c569d2e89a92b0
elsuizo <mnoblia@disroot.org>
2019-06-24 10:25:17 -0300

    matrix sum and product

--- src/matrix.rs
+++ src/matrix.rs
@@ -24,6 +24,10 @@
 //---------------------------------------------------------------------------
 
 use std::ops::{Deref, DerefMut};
+use num_traits::{One, Zero};
+
+use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Matrix3x3([[f64; 3]; 3]);
@@ -31,6 +35,59 @@ pub struct Matrix3x3([[f64; 3]; 3]);
 impl Matrix3x3 {
     pub fn new(data_input: [[f64; 3]; 3]) -> Matrix3x3 {
         Matrix3x3(data_input)
+    }
+}
+
+impl Add for Matrix3x3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Matrix3x3::new([
+            [self[0][0] + rhs[0][0], self[0][1] + rhs[0][1], self[0][2] + rhs[0][2]],
+            [self[1][0] + rhs[1][0], self[1][1] + rhs[1][1], self[1][2] + rhs[1][2]],
+            [self[2][0] + rhs[2][0], self[2][1] + rhs[2][1], self[2][2] + rhs[2][2]],
+        ])
+    }
+}
+
+impl Mul for Matrix3x3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        let m00 = self[0][0] * rhs[0][0] + self[0][1] * rhs[1][0] + self[0][2] * rhs[2][0];
+        let m01 = self[0][0] * rhs[0][1] + self[0][1] * rhs[1][1] + self[0][2] * rhs[2][1];
+        let m02 = self[0][0] * rhs[0][2] + self[0][1] * rhs[1][2] + self[0][2] * rhs[2][2];
+
+        let m10 = self[1][0] * rhs[0][0] + self[1][1] * rhs[1][0] + self[1][2] * rhs[2][0];
+        let m11 = self[1][0] * rhs[0][1] + self[1][1] * rhs[1][1] + self[1][2] * rhs[2][1];
+        let m12 = self[1][0] * rhs[0][2] + self[1][1] * rhs[1][2] + self[1][2] * rhs[2][2];
+
+        let m20 = self[2][0] * rhs[0][0] + self[2][1] * rhs[1][0] + self[2][2] * rhs[2][0];
+        let m21 = self[2][0] * rhs[0][1] + self[2][1] * rhs[1][1] + self[2][2] * rhs[2][1];
+        let m22 = self[2][0] * rhs[0][2] + self[2][1] * rhs[1][2] + self[2][2] * rhs[2][2];
+
+        Matrix3x3::new([
+            [m00, m01, m02],
+            [m10, m11, m12],
+            [m20, m21, m22],
+        ])
+    }
+}
+
+impl Zero for Matrix3x3 {
+    fn zero() -> Matrix3x3 {
+        Matrix3x3::new([[0.0; 3]; 3])
+    }
+
+    fn is_zero(&self) -> bool {
+        *self == Matrix3x3::zero()
+    }
+}
+
+impl One for Matrix3x3 {
+    /// Create an identity matrix
+    fn one() -> Matrix3x3 {
+        Matrix3x3::new([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
     }
 }
 
