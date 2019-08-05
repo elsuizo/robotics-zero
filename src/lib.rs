b9a3f93f1ff90ade3066ebcdc6f4689e3708f820
elsuizo <mnoblia@disroot.org>
2019-08-05 12:41:34 -0300

    more tests

--- src/lib.rs
+++ src/lib.rs
@@ -35,70 +35,69 @@ mod test_matrix {
         assert_eq!(matrix.trace(), 12.0);
     }
 
-}
-//     #[test]
-//     fn add_matrix() {
-//         let m1 = Matrix3x3::new([
-//                                     [0.0, 1.0, 2.0],
-//                                     [3.0, 4.0, 5.0],
-//                                     [6.0, 7.0, 8.0],
-//                                  ]);
-//
-//         let m2 = Matrix3x3::new([
-//                                     [0.0, 1.0, 2.0],
-//                                     [3.0, 4.0, 5.0],
-//                                     [6.0, 7.0, 8.0],
-//                                  ]);
-//
-//         let expected = Matrix3x3::new([
-//                                     [0.0, 2.0, 4.0],
-//                                     [6.0, 8.0, 10.0],
-//                                     [12.0, 14.0, 16.0],
-//                                  ]);
-//         let m3 = m1 + m2;
-//
+    #[test]
+    fn add_matrix() {
+        let m1 = Matrix3x3::new([
+                                    [0.0, 1.0, 2.0],
+                                    [3.0, 4.0, 5.0],
+                                    [6.0, 7.0, 8.0],
+                                 ]);
+
+        let m2 = Matrix3x3::new([
+                                    [0.0, 1.0, 2.0],
+                                    [3.0, 4.0, 5.0],
+                                    [6.0, 7.0, 8.0],
+                                 ]);
+
+        let expected = Matrix3x3::new([
+                                    [0.0, 2.0, 4.0],
+                                    [6.0, 8.0, 10.0],
+                                    [12.0, 14.0, 16.0],
+                                 ]);
+        let m3 = m1 + m2;
+
 //         // NOTE(elsuizo:2019-06-24): comparo usando slides
-//         assert_eq!(&m3[..], &expected[..], "\nExpected\n{:?}\nfound\n{:?}", &m3[..], &expected[..]);
-//     }
-//
-//     #[test]
-//     fn test_identity_creation() {
-//
-//         let I = Matrix3x3::identity();
-//
-//         let expected = Matrix3x3::new([
-//                                     [1.0, 0.0, 0.0],
-//                                     [0.0, 1.0, 0.0],
-//                                     [0.0, 0.0, 1.0],
-//                                  ]);
-//         assert_eq!(&I[..], &expected[..], "\nExpected\n{:?}\nfound\n{:?}", &I[..], &expected[..]);
-//     }
-//
-//     #[test]
-//     fn test_zeros_creation() {
-//         let zero = Matrix3x3::zeros();
-//
-//         let expected = Matrix3x3::new([
-//                                     [0.0, 0.0, 0.0],
-//                                     [0.0, 0.0, 0.0],
-//                                     [0.0, 0.0, 0.0],
-//                                     ]);
-//         assert_eq!(&zero[..], &expected[..], "\nExpected\n{:?}\nfound\n{:?}", &zero[..], &expected[..]);
-//     }
-//
-//     #[test]
-//     fn test_trace() {
-//         let m = Matrix3x3::identity();
-//         assert_eq!(m.trace(), 3.0);
-//     }
-//
-//     #[test]
-//     fn test_norm2() {
-//         let m = Matrix3x3::new([
-//                                     [0.0, 1.0, 2.0],
-//                                     [3.0, 4.0, 5.0],
-//                                     [6.0, 7.0, 8.0],
-//                                  ]);
-//         assert_ulps_eq!(m.norm2(), 14.2828568570857);
-//     }
-// }
+        assert_eq!(&m3[..], &expected[..], "\nExpected\n{:?}\nfound\n{:?}", &m3[..], &expected[..]);
+    }
+
+    #[test]
+    fn test_identity_creation() {
+
+        let I: Matrix3x3<f64> = Matrix3x3::identity();
+
+        let expected = Matrix3x3::new([
+                                    [1.0, 0.0, 0.0],
+                                    [0.0, 1.0, 0.0],
+                                    [0.0, 0.0, 1.0],
+                                 ]);
+        assert_eq!(&I[..], &expected[..], "\nExpected\n{:?}\nfound\n{:?}", &I[..], &expected[..]);
+    }
+
+    #[test]
+    fn test_zeros_creation() {
+        let zero: Matrix3x3<f64> = Matrix3x3::zeros();
+
+        let expected = Matrix3x3::new([
+                                    [0.0, 0.0, 0.0],
+                                    [0.0, 0.0, 0.0],
+                                    [0.0, 0.0, 0.0],
+                                    ]);
+        assert_eq!(&zero[..], &expected[..], "\nExpected\n{:?}\nfound\n{:?}", &zero[..], &expected[..]);
+    }
+
+    #[test]
+    fn test_trace() {
+        let m: Matrix3x3<f64> = Matrix3x3::identity();
+        assert_eq!(m.trace(), 3.0);
+    }
+
+    #[test]
+    fn test_norm2() {
+        let m = Matrix3x3::new([
+                                    [0.0, 1.0, 2.0],
+                                    [3.0, 4.0, 5.0],
+                                    [6.0, 7.0, 8.0],
+                                 ]);
+        assert_ulps_eq!(m.norm2(), 14.2828568570857);
+    }
+}
