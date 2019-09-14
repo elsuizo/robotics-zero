a9256b95b4210d430cd067e11edb7d038d0ae3db
elsuizo <mnoblia@disroot.org>
2019-09-13 23:37:06 -0300

    add `Vector4` type

--- src/vector4.rs
+++ src/vector4.rs
@@ -1,15 +1,15 @@
-//-------------------------------------------------------------------------
-// @file vector3.rs
+// -------------------------------------------------------------------------
+// @file vector4.rs
 //
-// @date 09/08/19 21:55:08
+// @date 09/13/19 20:36:22
 // @author Martin Noblia
 // @email mnoblia@disroot.org
 //
 // @brief
-// Static vector of three dimentions
+//
 // @detail
 //
-//  Licence:
+// Licence:
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or (at
@@ -21,37 +21,38 @@
 // General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-//--------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 // imports
 use std::ops::{Deref, DerefMut};
 use num_traits::{Zero, Float};
 
 use std::ops::{Add, Mul};
-// use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
 
-use crate::matrix3x3::Matrix3x3;
+use crate::matrix4x4::Matrix4x4;
+
 //-------------------------------------------------------------------------
 //                        code
 //-------------------------------------------------------------------------
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Vector3<T>([T; 3]);
+pub struct Vector4<T>([T; 4]);
 
-impl<T> Vector3<T> {
+impl<T> Vector4<T> {
     pub fn new(input: [T; 3]) -> Vector3<T> {
         Vector3(input)
     }
 }
 
-impl<T: Float> Vector3<T> {
-    pub fn zeros() -> Vector3<T> {
-        <Vector3<T> as Zero>::zero()
+impl<T: Float> Vector4<T> {
+    pub fn zeros() -> Vector4<T> {
+        <Vector4<T> as Zero>::zero()
     }
 
     pub fn norm2(&self) -> T {
         let a = self[0];
         let b = self[1];
         let c = self[2];
-        T::sqrt(a * a + b * b + c * c)
+        let d = self[3];
+        T::sqrt(a * a + b * b + c * c + d * d)
     }
 }
 
@@ -62,57 +63,72 @@ impl<T: Float> Mul for Vector3<T> {
         let a1 = self[0];
         let a2 = self[1];
         let a3 = self[2];
+        let a4 = self[3];
 
         let b1 = rhs[0];
         let b2 = rhs[1];
         let b3 = rhs[2];
+        let b4 = rhs[3];
 
-        a1 * b1 + a2 * b2 + a3 * b3
+        a1 * b1 + a2 * b2 + a3 * b3 + a4 * b4
     }
 }
 
-impl<T: Float> Mul<Matrix3x3<T>> for Vector3<T> {
+impl<T: Float> Mul<Matrix4x4<T>> for Vector4<T> {
     type Output = Vector3<T>;
 
-    fn mul(self, rhs: Matrix3x3<T>) -> Vector3<T> {
-        let a11 = rhs[(0, 0)];
-        let a12 = rhs[(0, 1)];
-        let a13 = rhs[(0, 2)];
-        let a21 = rhs[(1, 0)];
-        let a22 = rhs[(1, 1)];
-        let a23 = rhs[(1, 2)];
-        let a31 = rhs[(2, 0)];
-        let a32 = rhs[(2, 1)];
-        let a33 = rhs[(2, 2)];
+    fn mul(self, rhs: Matrix4x4<T>) -> Vector4<T> {
+        let a11  = rhs[(0, 0)];
+        let a12  = rhs[(0, 1)];
+        let a13  = rhs[(0, 2)];
+        let a14  = rhs[(0, 3)];
+        let a21  = rhs[(1, 0)];
+        let a22  = rhs[(1, 1)];
+        let a23  = rhs[(1, 2)];
+        let a24  = rhs[(1, 3)];
+        let a31  = rhs[(2, 0)];
+        let a32  = rhs[(2, 1)];
+        let a33  = rhs[(2, 2)];
+        let a34  = rhs[(2, 3)];
+        let a41  = rhs[(3, 0)];
+        let a42  = rhs[(3, 1)];
+        let a43  = rhs[(3, 2)];
+        let a44  = rhs[(3, 3)];
 
         let v1 = self[0];
         let v2 = self[1];
         let v3 = self[2];
+        let v4 = self[3];
 
-        Vector3::new([a11 * v1 + a12 * v2 + a13 * v3, a21 * v1 + a22 * v2 + a23 * v3, a31 * v1 + a32 * v2 + a33 * v3])
+        Vector4::new([v1 * a11 + v2 * a21 + v3 * a31 + v4 * a41,
+                      v1 * a12 + v2 * a22 + v3 * a32 + v4 * a42,
+                      v1 * a13 + v2 * a23 + v3 * a33 + v4 * a43,
+                      v1 * a14 + v2 * a24 + v3 * a34 + v4 * a44,])
     }
 }
 
-impl<T: Float> Add for Vector3<T> {
+impl<T: Float> Add for Vector4<T> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
         let v1 = self[0];
         let v2 = self[1];
         let v3 = self[2];
+        let v4 = self[3];
 
         let a1 = rhs[0];
         let a2 = rhs[1];
         let a3 = rhs[2];
+        let a4 = rhs[3];
 
-        Vector3::new([v1 + a1, v2 + a2, v3 + a3])
+        Vector3::new([v1 + a1, v2 + a2, v3 + a3, v4 + a4])
     }
 }
 
-impl<T: Float> Zero for Vector3<T> {
+impl<T: Float> Zero for Vector4<T> {
 
-    fn zero() -> Vector3<T> {
-        Vector3::new([T::zero(); 3])
+    fn zero() -> Vector4<T> {
+        Vector3::new([T::zero(); 4])
     }
 
     fn is_zero(&self) -> bool {
@@ -121,23 +137,23 @@ impl<T: Float> Zero for Vector3<T> {
 
 }
 
-impl<T> Deref for Vector3<T> {
-    type Target = [T; 3];
+impl<T> Deref for Vector4<T> {
+    type Target = [T; 4];
     #[inline]
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl<T> DerefMut for Vector3<T> {
+impl<T> DerefMut for Vector4<T> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
 }
 
-impl<T> From<[T; 3]> for Vector3<T> {
-    fn from(data: [T; 3]) -> Vector3<T> {
-        Vector3(data)
+impl<T> From<[T; 4]> for Vector4<T> {
+    fn from(data: [T; 4]) -> Vector4<T> {
+        Vector4(data)
     }
 }
